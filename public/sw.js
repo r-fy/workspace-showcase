3fe22411b8aaa1f0d62593bd722087ead2bcb372
Raffi <raffias@tuta.io>
2026-07-23 09:10:04 -0700

    Renumber dialer to v118 to avoid colliding with master's v117 (reminders early-complete)

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'workspace-v117';
-const SHELL = ['/', '/index.html', '/manifest.json', '/app.css?v=107', '/app.js?v=115', '/editor.bundle.js?v=66', '/dialer.bundle.js?v=1'];
+const CACHE = 'workspace-v118';
+const SHELL = ['/', '/index.html', '/manifest.json', '/app.css?v=107', '/app.js?v=116', '/editor.bundle.js?v=66', '/dialer.bundle.js?v=1'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
