94ad5ceb48436839271dda6bd9a557704f5495d5
Raffi <raffias@tuta.io>
2026-07-26 19:25:14 -0700

    Bump cache versions for Daily SEO Task category deploy

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'workspace-v143';
-const SHELL = ['/', '/index.html', '/manifest.json', '/app.css?v=124', '/app.js?v=140', '/editor.bundle.js?v=66', '/dialer.bundle.js?v=1'];
+const CACHE = 'workspace-v144';
+const SHELL = ['/', '/index.html', '/manifest.json', '/app.css?v=124', '/app.js?v=141', '/editor.bundle.js?v=66', '/dialer.bundle.js?v=1'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
